8553c71abbfc9d06c5e707c646abbe6f075c0d59
unknown <samiraborsha08@gmail.com>
2026-01-09 02:28:55 +0600

    feat: mongoose, zod error handling done

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -2,6 +2,11 @@
 import { NextFunction, Request, Response } from "express";
 import { envVars } from "../config/env";
 import { deleteImageFromCloudinary } from "../config/cloudinary.config";
+import { handleValidationError } from "../errorHelpers/handleValidationError";
+import { IErrorSources } from "../types/error.type";
+import { handleCastError } from "../errorHelpers/handleCastError";
+import { handleZodError } from "../errorHelpers/handleZodError";
+import AppError from "../errorHelpers/AppError";
 
 export const globalErrorHandler = async (
     err: any,
@@ -9,8 +14,13 @@ export const globalErrorHandler = async (
     res: Response,
     next: NextFunction
 ) => {
+
+    let statusCode = 500;
+    let message = "Something went wrong";
+    let errorSources: IErrorSources[] = [];
+
     if (envVars.NODE_ENV === "development") {
-        console.log("❌ Global Error: ", err);
+        console.error("❌ Global Error: ", err.message);
     }
 
     if (req.file) {
@@ -22,12 +32,33 @@ export const globalErrorHandler = async (
         await Promise.all(imageUrls.map((url) => deleteImageFromCloudinary(url)));
     }
 
-    let statusCode = 500;
-    let message = "Something Went Wrong!!";
+    if (err.name === "ValidationError") {
+        const simplifiedError = handleValidationError(err, errorSources);
+        statusCode = simplifiedError.statusCode;
+        message = simplifiedError.message;
+        errorSources = simplifiedError.errorSources as IErrorSources[];
+    }
+    if (err.name === "CastError") {
+        const simplifiedError = handleCastError();
+        statusCode = simplifiedError.statusCode;
+        message = simplifiedError.message;
+    } else if (err.name === "ZodError") {
+        const simplifiedZodError = handleZodError(err, errorSources);
+        statusCode = simplifiedZodError.statusCode;
+        message = simplifiedZodError.message;
+        errorSources = simplifiedZodError.errorSources as IErrorSources[];
+    } else if (err instanceof AppError) {
+        statusCode = err.statusCode;
+        message = err.message;
+    } else if (err instanceof Error) {
+        statusCode = 500;
+        message = err.message;
+    }
 
     res.status(statusCode).json({
         success: false,
         message,
+        errorSources,
         err: envVars.NODE_ENV === "development" ? err : null,
         stack: envVars.NODE_ENV === "development" ? err.stack : null,
     });
